f8cd4c55284d09b73c342c70e4ca1db49b0bcfbf
xkylz1 <fauzansaputra741@gmail.com>
2024-10-11 23:08:32 +0800

    single file upload

---
+++ middlewares/uploader.js
@@ -0,0 +1,15 @@
+const multer = require("multer");
+
+const multerFiltering = (req, file, cb) => {
+  if (file.mimetype == "image/png" || file.mimetype == "image/jpg") {
+    cb(null, true);
+  } else {
+    throw new Error("image format is not valid...");
+  }
+};
+
+const upload = multer({
+  fileFilter: multerFiltering
+});
+
+module.exports = upload;
